a0e0c1d0403f42e3a5c7777fcc56f0921cd73bd5
Julien-ABT <37828211+Julien-ABT@users.noreply.github.com>
2018-09-19 06:39:53 +0200

    Update JsonHandler with correction because eclipse bugging at home

--- src/application/JSonHandler.java
+++ src/application/JSonHandler.java
@@ -2,58 +2,68 @@ package application;
 
 import java.io.IOException;
 import java.io.StringWriter;
-import java.util.ArrayList;
-
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JSonHandler {
-
+	private ObjectMapper objectMapper = new ObjectMapper();
+	
 	public String userToJson(User user){
-		//create ObjectMapper instance
-		ObjectMapper objectMapper = new ObjectMapper();
 		StringWriter jsonString = new StringWriter();
 		
 		try {
 			objectMapper.writeValue(jsonString, user);
-		} catch (JsonGenerationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 		return jsonString.toString();
 	}
 	
 	public User jsonToUser(String json){
-		ObjectMapper objectMapper = new ObjectMapper();
-		User user =null;
+		User user;
+
+		json = extractData(json);
+				
 		try {
 			user = objectMapper.readValue(json, User.class);
-		} catch (JsonParseException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (JsonMappingException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			user = null;
+		} catch (NullPointerException nullExcep){
+			System.out.println("Field 'data' does not exist, generating a new user with no data");
+			user = new User();
 		}
+		
 		return user;
 	}
 	
-	public ArrayList<User> jsonToUsers(String json){
+	public User[] jsonToUsers(String json){
+		User[] list;
 		
+		json = extractData(json);
 		
+		try {
+			list = objectMapper.readValue(json, User[].class);
+		} catch (IOException e) {
+			list = null;
+		}
 		
-		return null;
+		return list;
+	}
+	
+	private String extractData(String jsonIn){
+		String jsonOut;
+		
+		try {
+			jsonOut = objectMapper.readTree(jsonIn).get("data").asText();
+		} catch (NullPointerException e1) {
+			e1.printStackTrace();
+			jsonOut = "{}";
+		} catch (IOException e) {
+			e.printStackTrace();
+			jsonOut = "{}";
+		}
+		
+		return jsonOut;
 	}
 }
-
