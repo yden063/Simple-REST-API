cdcd838c238678c231e60b376a821398265fb19b
Youness Dendane <youness.dendane1@uqac.ca>
2018-09-18 20:23:34 -0400

    Designed the UI and removed .classpath and application.css

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,21 +1,29 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
+		Parent root;
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			root = FXMLLoader.load(getClass().getResource("DisplayDataView.fxml"));
+			
+			Scene scene = new Scene(root); 
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("API Test with Java & JavaFX");
+			
 			primaryStage.show();
-		} catch(Exception e) {
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
